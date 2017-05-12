afb5e7bad23538f984bca9a7e9e32f2d8962baca
Neil Ashford <ashfordneil0@gmail.com>
2017-05-12 22:25:08 +1000

    stored greps in a HashSet rather than a vector -- I should have done this from the start but I didn't want to manually create implement all the traits

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+#![feature(retain_hash_collection)]
 extern crate discord;
 extern crate regex;
 
 use std::collections::{HashSet, HashMap};
 use std::env;
+use std::cmp::{PartialEq, Eq};
+use std::hash::{Hash, Hasher};
 use std::time::{Duration, Instant};
 
 use discord::Discord;
@@ -12,8 +15,31 @@ use regex::Regex;
 
 const TIMEOUT: u64 = 5 * 60; // 5 minutes
 
+/// Wrapper around a `Regex, UserId` tuple that implements `PartialEq`, `Eq` and `Hash` manually,
+/// using the `Regex::as_str()` function as the `Regex` object itself cannot be hashed.
+struct Grep(Regex, UserId);
+
+impl PartialEq<Grep> for Grep {
+    fn eq(&self, other: &Self) -> bool {
+        let Grep(ref regex, id) = *self;
+        let Grep(ref other_regex, other_id) = *other;
+
+        regex.as_str() == other_regex.as_str() && id == other_id
+    }
+}
+
+impl Eq for Grep {}
+
+impl Hash for Grep {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        let Grep(ref regex, id) = *self;
+        regex.as_str().hash(state);
+        id.hash(state);
+    }
+}
+
 fn handle_message(message: Message,
-                  greps: &mut Vec<(Regex, UserId)>,
+                  greps: &mut HashSet<Grep>,
                   timeouts: &mut HashMap<(UserId, ChannelId), Instant>)
                   -> Option<String> {
     let channel = message.channel_id;
@@ -28,18 +54,18 @@ fn handle_message(message: Message,
         Some(include_str!("help.md").into())
     } else if content == "!mygreps" {
         Some(greps.iter()
-            .filter(|&&(_, id)| id == author.id)
-            .map(|&(ref regex, _)| regex)
+            .filter(|&&Grep(_, id)| id == author.id)
+            .map(|&Grep(ref regex, _)| regex)
             .fold(String::new(),
                   |string, regex| format!("{}\n{}", string, regex)))
     } else if content.starts_with("!grep ") {
         content.splitn(2, ' ').nth(1).map(|pattern| match Regex::new(pattern) {
             Ok(regex) => {
                 if greps.iter()
-                    .any(|&(ref regex, id)| id == author.id && regex.as_str() == pattern) {
+                    .any(|&Grep(ref regex, id)| id == author.id && regex.as_str() == pattern) {
                     "Regex already exists".into()
                 } else {
-                    greps.push((regex, author.id));
+                    greps.insert(Grep(regex, author.id));
                     "Regex added".into()
                 }
             }
@@ -48,7 +74,7 @@ fn handle_message(message: Message,
     } else if content.starts_with("!ungrep ") {
         content.splitn(2, ' ').nth(1).map(|pattern| {
             let mut removals = false;
-            greps.retain(|&(ref regex, id)| {
+            greps.retain(|&Grep(ref regex, id)| {
                 if id == author.id && regex.as_str() == pattern {
                     removals = true;
                     false
@@ -64,8 +90,8 @@ fn handle_message(message: Message,
         })
     } else {
         let users: HashSet<_> = greps.iter()
-            .filter(|&&(ref regex, _)| regex.is_match(&content))
-            .map(|&(_, id)| id)
+            .filter(|&&Grep(ref regex, _)| regex.is_match(&content))
+            .map(|&Grep(_, id)| id)
             .filter(|&id| id != author.id)
             .filter(|&id| match timeouts.get(&(id, channel)) {
                 Some(instant) => instant.elapsed() > Duration::from_secs(TIMEOUT),
@@ -87,7 +113,7 @@ fn handle_message(message: Message,
 
 fn main() {
     // state
-    let mut greps = Vec::new();
+    let mut greps = HashSet::new();
     let mut timeouts = HashMap::new();
     // api
     let discord = Discord::from_bot_token(&env::var("DISCORD_BOT_TOKEN")
